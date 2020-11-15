338e7a83a49735fb20c673b62e26679fc780e608
Mahmud <mmnabi.cse@gmail.com>
2020-11-15 13:53:30 +0600

    Untester Initialization

---
+++ Models/DB/UMS_APPCONFIG.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.DB
+{
+    public class UMS_APPCONFIG
+    {
+        string PKVALUE { get; set; }
+        int MAXAUTHLEVEL { get; set; }
+        int APPAUTHLEVEL { get; set; }
+        string USERID { get; set; }
+        string ACTION { get; set; }
+        string REMARK { get; set; }
+        string NEWRECORD { get; set; }
+        string OLDRECORD { get; set; }
+        string OLDSTATUS { get; set; }
+        string RECSTATUS { get; set; }
+        string URLLINK { get; set; }
+        string ISAUTHCANCEL { get; set; }
+    }
+}
